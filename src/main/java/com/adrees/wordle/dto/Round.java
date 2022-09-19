6e83e67e331d4dcf26314f23cde7f4a9994d34c7
Angela Drees <drees.angie@gmail.com>
2022-09-19 09:52:45 -0500

    Add DTO

---
+++ src/main/java/com/adrees/wordle/dto/Round.java
@@ -0,0 +1,81 @@
+
+package com.adrees.wordle.dto;
+
+import java.util.Objects;
+
+public class Round {
+    
+    private int roundId;
+    private int gameId;
+    private String guess;
+    private String result;
+
+    public int getRoundId() {
+        return roundId;
+    }
+
+    public void setRoundId(int roundId) {
+        this.roundId = roundId;
+    }
+
+    public int getGameId() {
+        return gameId;
+    }
+
+    public void setGameId(int gameId) {
+        this.gameId = gameId;
+    }
+
+    public String getGuess() {
+        return guess;
+    }
+
+    public void setGuess(String guess) {
+        this.guess = guess;
+    }
+
+    public String getResult() {
+        return result;
+    }
+
+    public void setResult(String result) {
+        this.result = result;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 41 * hash + this.roundId;
+        hash = 41 * hash + this.gameId;
+        hash = 41 * hash + Objects.hashCode(this.guess);
+        hash = 41 * hash + Objects.hashCode(this.result);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Round other = (Round) obj;
+        if (this.roundId != other.roundId) {
+            return false;
+        }
+        if (this.gameId != other.gameId) {
+            return false;
+        }
+        if (!Objects.equals(this.guess, other.guess)) {
+            return false;
+        }
+        return Objects.equals(this.result, other.result);
+    }
+    
+    
+    
+}
