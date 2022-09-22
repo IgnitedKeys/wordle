beb20ffee8403493d67487b8e919218a6afd7e1d
Angela Drees <drees.angie@gmail.com>
2022-09-22 11:31:27 -0500

    dao tests created

---
+++ src/test/java/com/adrees/wordle/dao/GameDBDaoIT.java
@@ -0,0 +1,74 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.adrees.wordle.dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author adrees
+ */
+public class GameDBDaoIT {
+    
+    public GameDBDaoIT() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of add method, of class GameDBDao.
+     */
+    @Test
+    public void testAdd() {
+    }
+
+    /**
+     * Test of getAllGames method, of class GameDBDao.
+     */
+    @Test
+    public void testGetAllGames() {
+    }
+
+    /**
+     * Test of getGameById method, of class GameDBDao.
+     */
+    @Test
+    public void testGetGameById() {
+    }
+
+    /**
+     * Test of updateGame method, of class GameDBDao.
+     */
+    @Test
+    public void testUpdateGame() {
+    }
+
+    /**
+     * Test of deleteGameById method, of class GameDBDao.
+     */
+    @Test
+    public void testDeleteGameById() {
+    }
+    
+}
---
+++ src/test/java/com/adrees/wordle/dao/RoundDBDaoIT.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit5TestClass.java to edit this template
+ */
+package com.adrees.wordle.dao;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ *
+ * @author adrees
+ */
+public class RoundDBDaoIT {
+    
+    public RoundDBDaoIT() {
+    }
+    
+    @BeforeAll
+    public static void setUpClass() {
+    }
+    
+    @AfterAll
+    public static void tearDownClass() {
+    }
+    
+    @BeforeEach
+    public void setUp() {
+    }
+    
+    @AfterEach
+    public void tearDown() {
+    }
+
+    /**
+     * Test of addRound method, of class RoundDBDao.
+     */
+    @Test
+    public void testAddRound() {
+    }
+
+    /**
+     * Test of getRoundById method, of class RoundDBDao.
+     */
+    @Test
+    public void testGetRoundById() {
+    }
+
+    /**
+     * Test of roundsForGame method, of class RoundDBDao.
+     */
+    @Test
+    public void testRoundsForGame() {
+    }
+
+    /**
+     * Test of deleteRound method, of class RoundDBDao.
+     */
+    @Test
+    public void testDeleteRound() {
+    }
+    
+}
